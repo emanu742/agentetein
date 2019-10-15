c6f0b359339239971ead261006e0a8465530aae1
emanu742 <49461363+emanu742@users.noreply.github.com>
2019-10-14 22:06:09 -0300

    Update cadastroSaude.php

--- cadastroSaude.php
+++ cadastroSaude.php
@@ -24,7 +24,7 @@ if (isset($_POST)):
         // Grava no banco de dados as informa��es do contato
         $opcoes = array(\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8');
 
-        $pdo = new PDO('mysql:host=localhost;dbname=db_blog', 'root', '', $opcoes);
+        $pdo = new PDO('mysql:host=localhost;dbname='id11181194_agentedb', 'id1181194_emanuel', 'emanuel123', $opcoes);
 
         $sql = "INSERT INTO saude (sus, doencaCardiaca, peso, doencaRespiratoria, problemaRins, fumante, mudanca, alcool, diabetes, drogas, avcDerrame, hipertenso, cancer)VALUES(:sus, :doencaCardiaca, :peso, :doencaRespiratoria, :problemaRins, :fumante, :mudanca, :alcool, :diabetes, :drogas, :avcDerrame, :hipertenso, :cancer)";
         $stm = $pdo->prepare($sql);
